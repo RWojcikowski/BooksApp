0463e0086afdb80be5262188851629345b79622d
RWojcikowski <Radoslaw.chicqen@gmail.com>
2021-07-31 17:53:48 +0200

    task 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,68 @@
-{
-    'use strict';
+// {
+//   'use strict';
 
-    const select = {
-        templatesOf: {
-            book: '#template-book',
-        },
-        listOf: {
-            books: '.books-panel .books-list',
-        }
-    };
-    const classNames = {
-        book: {
-            favoriteImage: 'favorite',
-        },
-    };
+//   const select = {
+//     templatesOf: {
+//       book: '#template-book',
+//     },
+//     listOf: {
+//       books: '.books-panel .books-list',
+//     }
+//   };
+//   const classNames = {
+//     book: {
+//       favoriteImage: 'favorite',
+//     },
+//   };
 
-    const templates = {
-        bookLink: Handlebars.compile(document.querySelector(select.templatesOf.book).innerHTML),
-    };
-
-
-    const render = function () {
-
-        for (let book in dataSource.books) {
-            const generatedHTML = templates.bookLink(dataSource.books[book]);
-            //console.log(`generatedHTML:`, generatedHTML);
-            book = utils.createDOMFromHTML(generatedHTML);
-            console.log(`bookHTML:`, book);
-            const booksList = document.querySelector(select.listOf.books);
-
-            booksList.appendChild(book);
-        }
-
-    };
-
-    render();
-
-    const favouriteBooks = [];
-    const initActions = function () {
-
-        bookImage.addEventlistener('dbclick', function (event) {
-            event.preventDefault();
-
-            const bookId = bookImage.getAttribute(select.bookProperties.id);
-            console.log(bookId);
-
-            if (!bookImage.classList.contains(classNames.book.favoriteImage)) {
-                bookImage.classList.add(classNames.book.favoriteImage);
-                favouriteBooks.push(bookImage);
-            } else if (bookImage.classList.contains(classNames.book.favoriteImage)) {
-                bookImage.classList.remove(classNames.book.favoriteImage);
-                favouriteBooks.splice(favouriteBooks.indexOf(bookImage), 1);
-            }
-            console.log(favouriteBooks);
-        });
-    };
+//   const templates = {
+//     bookLink: Handlebars.compile(document.querySelector(select.templatesOf.book).innerHTML),
+//   };
 
 
-    initActions();
+//   const render = function () {
+
+//     for (let book in dataSource.books) {
+//       const generatedHTML = templates.bookLink(dataSource.books[book]);
+//       //console.log(`generatedHTML:`, generatedHTML);
+//       book = utils.createDOMFromHTML(generatedHTML);
+//       console.log(`bookHTML:`, book);
+//       const booksList = document.querySelector(select.listOf.books);
+
+//       booksList.appendChild(book);
+//     }
+
+//   };
+
+//   render();
+
+//   const favouriteBooks = [];
+//   const initActions = function () {
+//     const bookImages = document.querySelectorAll('.books-list  .book__image');
+//     console.log(bookImages);
+//     for (let bookImage of bookImages) {
+//       console.log(bookImage);
+//       bookImage.addEventListener('dblclick', function (event) {
+//         event.preventDefault();
+
+//         const bookId = bookImage.getAttribute('data-id');
+//         console.log(bookId);
+
+//         if (!bookImage.classList.contains(classNames.book.favoriteImage)) {
+//           bookImage.classList.add(classNames.book.favoriteImage);
+//           favouriteBooks.push(bookImage);
+//         } else if (bookImage.classList.contains(classNames.book.favoriteImage)) {
+//           bookImage.classList.remove(classNames.book.favoriteImage);
+//           favouriteBooks.splice(favouriteBooks.indexOf(bookImage), 1);
+//         }
+//         console.log(favouriteBooks);
+//       });
+//     }
+//   };
 
 
-}
+//   initActions();
+
+
+// }
 
